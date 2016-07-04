06d2ffc2ae92abba3b8de58dca480bdaa5c6d076
air1bzz <air1bzz@gmail.com>
2016-07-04 23:01:45 +0200

    Add files & comments

--- lib/organ_cooker.rb
+++ lib/organ_cooker.rb
@@ -1,5 +1,9 @@
-require "organ_cooker/version"
+require 'organ_cooker/version'
+require 'organ_cooker/project'
+require 'organ_cooker/manual'
+require 'organ_cooker/rank'
 
+# Main module for this gem.
 module OrganCooker
   # Your code goes here...
 end
